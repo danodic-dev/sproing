75eb94b004e16a459e9c140bafb1a8b91b89c40b
danodic <danilo@danodic.dev>
2023-08-07 23:30:38 -0300

    Add dependency validation.

--- container.py
+++ container.py
@@ -1,7 +1,8 @@
 from collections import deque
+from dataclasses import dataclass
 from inspect import getargspec, signature, Parameter
 from types import MappingProxyType
-from typing import Callable, Type, get_type_hints, Dict, Any
+from typing import Callable, Type, get_type_hints, Dict, Any, List
 
 dependencies = {}
 
@@ -43,17 +44,40 @@ def register_dependency(return_type: Type,
     dependencies[return_type].append(Dependency(provider))
 
 
-def is_valid_dependency(fn: Callable) -> bool:
+@dataclass
+class ArgumentError:
+    argument_name: str
+    dependency_name: str
+    error_message: str
+
+
+@dataclass
+class ReturnError:
+    dependency_name: str
+    error_message: str
+
+
+def validate_dependencies(fn: Callable) -> List[ArgumentError | ReturnError]:
     def is_valid_argument(argument: Parameter,
                           hints: Dict[str, Any]):
         return argument.name in hints
 
     def validate_arguments(arguments: MappingProxyType[str, Parameter],
                            hints: Dict[str, Any]):
-        validation = {}
+        validations = {}
         for argument in arguments.values():
-            validation[argument.name] = is_valid_argument(argument, hints)
-        return validation
+            validations[argument.name] = is_valid_argument(argument, hints)
+        return validations
+
+    def argument_validation_error(arg_name: str,
+                                  dep_name: str) -> ArgumentError:
+        return ArgumentError(argument_name=arg_name,
+                             dependency_name=dep_name,
+                             error_message=f"Dependency '{dep_name}' does not have a type hint for the argument '{arg_name}'.")
+
+    def return_validation_error(dep_name: str) -> ReturnError:
+        return ReturnError(dependency_name=dep_name,
+                           error_message=f"Dependency '{dep_name}' does not have a type hint for its return type.")
 
     def has_valid_return(hints: Dict[str, Any]):
         return 'return' in hints
@@ -61,24 +85,36 @@ def is_valid_dependency(fn: Callable) -> bool:
     hints = get_type_hints(fn)
     sign = signature(fn)
 
+    dependency_name = fn.__name__
+    validation_errors = []
     validation = validate_arguments(sign.parameters, hints)
-    #if not all([])
+    if not all(validation.values()):
+        for arg, result in validation:
+            if not result:
+                error = argument_validation_error(arg, dependency_name)
+                validation_errors.append(error)
 
+    if not has_valid_return(hints):
+        validation_errors.append(return_validation_error(dependency_name))
+
+    return validation_errors
 
 
 def dependency(fn: Callable):
+    if errors := validate_dependencies(fn):
+        raise RuntimeError(str(errors))
     hints = get_type_hints(fn)
     register_dependency(hints['return'], fn)
     return fn
 
 
-def dep_lookup(dep_type: typing.Type) -> Dependency:
-    return dependencies[dep_type]
+def dep_lookup(dep_type: Type) -> Dependency:
+    return dependencies[dep_type][0]
 
 
 def inject(fn: Callable):
     def wrapper():
-        kwargs = typing.get_type_hints(fn)
+        kwargs = get_type_hints(fn)
         if 'return' in kwargs:
             del kwargs['return']
         kwargs = {key: dep_lookup(value)() for key, value in kwargs.items()}
