dcf3cc03974aac9e8a527b4699b95a2cf1348374
danodic <danilo@danodic.dev>
2023-08-11 02:28:14 -0300

    #4 Register a list of dependencies.

--- sproing/container.py
+++ sproing/container.py
@@ -2,16 +2,18 @@ from typing import Callable, Dict, Type, get_type_hints, Any, List
 
 from sproing.dependency import SproingDependency
 
-dependencies: Dict[Type, SproingDependency] = {}
+DEPENDENCIES_TYPE = Dict[Type, List[SproingDependency]]
+
+dependencies: DEPENDENCIES_TYPE = {}
 
 
 def get_return_type(fn_: Callable):
     return get_type_hints(fn_)['return']
 
 
-def register_dependency(fn: Callable) -> Dict[Any, SproingDependency]:
+def register_dependency(fn: Callable) -> DEPENDENCIES_TYPE:
     dependency = SproingDependency(fn)
-    dependencies[dependency.return_type()] = dependency
+    dependencies.setdefault(dependency.return_type(), []).append(dependency)
     return dependencies
 
 
@@ -24,4 +26,4 @@ class NoSuchSproingDependency(Exception):
 def get_dependency(dependency_type: Type = None) -> SproingDependency:
     if dependency_type not in dependencies:
         raise NoSuchSproingDependency(dependency_type)
-    return dependencies[dependency_type]
+    return dependencies[dependency_type][0]
